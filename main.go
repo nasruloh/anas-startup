55edde3daae297f5ebda3d0a987dede5879b1930
sejahterastore <sejahterastore0@gmail.com>
2023-10-23 23:37:34 +0700

    campaign repository

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"startup-anas/auth"
+	"startup-anas/campaign"
 	"startup-anas/handler"
 	"startup-anas/helper"
 	"startup-anas/user"
@@ -27,6 +29,24 @@ func main() {
 	}
 
 	userRepository := user.NewRepository(db)
+	campaignRepository := campaign.NewRepository(db)
+
+	campaigns , err:= campaignRepository.FindByUserID(1)
+
+	fmt.Println("debug")
+	fmt.Println("debug")
+	fmt.Println("debug")
+	fmt.Println(len(campaigns))
+	for _, campaign := range campaigns{
+		fmt.Println(campaign.Name)
+		if len(campaign.CampaignImages) >0{
+			fmt.Println("jumlah gambar")
+			fmt.Println(len(campaign.CampaignImages))
+			fmt.Println(campaign.CampaignImages[0].FileName)
+		}
+		
+	}
+
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
